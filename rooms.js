0a3a133ea9d7700cab945d05577d69d9d1163121
Nikolas Silva <nikolas.rsilva@gmail.com>
2014-08-01 02:52:19 -0300

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -317,7 +317,7 @@ var GlobalRoom = (function () {
 
 		if (!this.pastSearchTimes) this.pastSearchTimes = {};
 		if (!this.pastSearchTimes[newSearch.formatid] || Date.now() > this.pastSearchTimes[newSearch.formatid] + (60).seconds()) {
-			getRoom('lobby').add("|raw|<em>Se está buscando una batalla de <strong><font color=#8A0808>" + Tools.escapeHTML(Tools.getFormat(newSearch.formatid).name) + "</font></strong> en la ladder.</em>");
+			getRoom('lobby').add("|raw|<em>Se está buscando una batalla de <strong><font color=#0058CC>" + Tools.escapeHTML(Tools.getFormat(newSearch.formatid).name) + "</font></strong> en la ladder.</em>");
 		}
 		this.pastSearchTimes[newSearch.formatid] = Date.now();
 	};
